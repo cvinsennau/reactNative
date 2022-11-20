cf49190ca8ca8c9f369d37fae241372468573303
MauroOsso <mosso@udesa.edu.ar>
2022-11-20 20:49:14 -0300

    Buscador Maquetado

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react" ;
-import {Text, View, StyleSheet, TextInput, FlatList } from 'react-native'
+import {Text, View, StyleSheet, TextInput, FlatList, Button, Keyboard } from 'react-native'
 import { db, auth } from '../firebase/config';
+import { Feather, Entypo } from "@expo/vector-icons";
 
 class Search extends Component {
     constructor(){
@@ -52,8 +53,28 @@ class Search extends Component {
     }     
 
     render(){
+        const SearchBar = ({clicked, searchPhrase, setSearchPhrase, setCLicked}) => {
         return(
-            <View style={styles.container} >                
+
+            <View style={styles.container}>
+                <View style={clicked ? styles.searchBar__clicked : styles.searchBar__unclicked}> 
+
+                    {/* search Icon */}
+                    <Feather name="search" size={20} color="black" style={{ marginLeft: 1 }}/> 
+
+                    {/* Input field */}
+                    <TextInput style={styles.input} placeholder="Search" value={searchPhrase} onChangeText={setSearchPhrase}onFocus={() => {setClicked(true);}}/>
+
+                    {/* cross Icon, depending on whether the search bar is clicked or not */}
+                    {clicked && (<Entypo name="cross" size={20} color="black" style={{ padding: 1 }} onPress={() => {setSearchPhrase("")}}/>)}
+        
+                </View>
+
+                    {/* cancel button, depending on whether the search bar is clicked or not */}
+                    {clicked && (<View> <Button title="Cancel" onPress={() => {Keyboard.dismiss();setClicked(false);}}></Button> </View>)}
+
+
+                           
                 <View >
                     <TextInput style={styles.input} 
                             placeholder= 'Busca un Usuario'
@@ -77,7 +98,7 @@ class Search extends Component {
                 }
             </View>
 
-        )
+        )};
     }
 }
 
@@ -86,18 +107,37 @@ export default Search;
 // Styles
 const styles = StyleSheet.create({
 
+    
     container: {
         margin: 15,
         justifyContent: "flex-start",
         alignItems: "center",
         flexDirection: "row",
         width: "90%",
+    
+      },
+      searchBar__unclicked: {
+        padding: 10,
+        flexDirection: "row",
+        width: "95%",
+        backgroundColor: "#d9dbda",
+        borderRadius: 15,
+        alignItems: "center",
+      },
+      searchBar__clicked: {
+        padding: 10,
+        flexDirection: "row",
+        width: "80%",
+        backgroundColor: "#d9dbda",
+        borderRadius: 15,
+        alignItems: "center",
+        justifyContent: "space-evenly",
+      },
+      input: {
+        fontSize: 20,
+        marginLeft: 10,
+        width: "90%",
       },
 
-input: {
-    fontSize: 20,
-    marginLeft: 10,
-    width: "90%",
-  },
 
 });
